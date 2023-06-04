abfb662cbc127039c89103b2a4797cb4cce186b0
vikas9620 <53708079+vikas9620@users.noreply.github.com>
2023-06-04 22:01:19 +0530

    Auto logout

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 
 const AuthContext = React.createContext({
   token: "",
@@ -9,6 +9,8 @@ const AuthContext = React.createContext({
 
 export const AuthContextProvider = (props) => {
   const initialToken = localStorage.getItem('token')
+  const [lastActiveTime, setLastActiveTime] = useState(Date.now());
+
   const [token, setToken] = useState(initialToken);
   const userIsLoggedIn = !!token;
 
@@ -21,7 +23,37 @@ export const AuthContextProvider = (props) => {
     setToken(null);
 localStorage.removeItem('token');
   };
-  
+  useEffect(() => {
+    const handleUserActivity = () => {
+      setLastActiveTime(Date.now());
+    };
+
+    // Attach event listeners for user activity
+    window.addEventListener("mousemove", handleUserActivity);
+    window.addEventListener("keypress", handleUserActivity);
+
+    return () => {
+      // Clean up event listeners on component unmount
+      window.removeEventListener("mousemove", handleUserActivity);
+      window.removeEventListener("keypress", handleUserActivity);
+    };
+  }, []);
+
+  useEffect(() => {
+    const inactivityPeriod = 5 * 60 * 1000; // 5 minutes in milliseconds
+
+    const checkInactivity = setInterval(() => {
+      if (Date.now() - lastActiveTime >= inactivityPeriod) {
+        logoutHandler();
+        alert("Your session has expired due to inactivity. Please log in again.");
+        clearInterval(checkInactivity);
+      }
+    }, 1000); // Check every second
+
+    return () => {
+      clearInterval(checkInactivity);
+    };
+  }, [lastActiveTime]);
 
   const contextValue = {
     token: token,
